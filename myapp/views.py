b136e4485cd32782fc115a54853a3efc676817b3
leoseis <eboscrib2@yahoo.com>
2023-11-07 14:22:47 +0100

    len check

--- myapp/views.py
+++ myapp/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def index(request):
     return render (request, 'index.html')
+
+
+def counter(request):
+    text = request.GET['text']
+    amount_of_words = len(text.split())
+    return render(request, 'counter.html',{'amount' : amount_of_words} )
